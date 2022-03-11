47cede123cbe891edef50e578f7e045657391f14
Pablo Azcona <pablo.azcona@blach.com>
2022-03-10 18:02:23 -0800

    error for pymongo

--- scraping.py
+++ scraping.py
@@ -3,12 +3,28 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
+import datetime as dt
 
 #Integrate MongoDB into the Web App
 def scrape_all():
     # Set up Splinter/Initiate driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
 
@@ -40,7 +56,7 @@ def mars_news(browser):
 
 # ## JPL Space Images Featured Image
 
-def featured_image(browser)
+def featured_image(browser):
 
     # Visit URL
     url = 'https://spaceimages-mars.com'
@@ -82,4 +98,6 @@ def mars_facts():
     #Convert dataframe into HTML format, add  bootstrap
     return df.to_html()
 
-browser.quit()
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())

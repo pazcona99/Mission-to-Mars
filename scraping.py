e1181100b41ee2b2ddd8e65c79962d600e391e1f
Pablo Azcona <pablo.azcona@blach.com>
2022-03-10 17:48:30 -0800

    modified scraping

--- scraping.py
+++ scraping.py
@@ -4,64 +4,82 @@ from bs4 import BeautifulSoup as soup
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+#Integrate MongoDB into the Web App
+def scrape_all():
+    # Set up Splinter/Initiate driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-# Visit the Mars news site
-url = 'https://redplanetscience.com/'
-browser.visit(url)
+def mars_news(browser):
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # Visit the Mars news site
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
 
-# Convert the browser html to a soup object and then quit the browser
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-slide_elem = news_soup.select_one('div.list_text')
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-slide_elem.find('div', class_='content_title')
+    #Add try/except for error handling prior to scraping
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
 
-# Use the parent element to find the first a tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+        # Use the parent element to find the first a tag and save it as `news_title`
+        news_title = slide_elem.find('div', class_='content_title').get_text()
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None, None
+
+    return news_title, news_p
 
 # ## JPL Space Images Featured Image
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+def featured_image(browser)
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Use the base url to create an absolute url
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    #Error handling prior to scraping
+    try:
+        # find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+    except AttributeError:
+        return None
+
+    # Use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+
+    return img_url
 
 # ## Mars Facts
+def mars_facts():
+    #Add try/except for error handling
+    try:
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    except BaseException:
+        return None
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.head()
+    #Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
-df.columns=['Description', 'Mars', 'Earth']
-df.set_index('Description', inplace=True)
-df
-
-df.to_html()
+    #Convert dataframe into HTML format, add  bootstrap
+    return df.to_html()
 
 browser.quit()
